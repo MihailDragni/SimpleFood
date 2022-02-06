5717d1afda655795ff4d714e13c59830050aa7b2
MihailDragni <74327586+MihailDragni@users.noreply.github.com>
2022-02-07 01:45:56 +0200

    added swiper added effects fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,23 @@
 $(function(){
-  
-  // $('.top-slider__inner').slick({
-  //   dots: true,
-  //   arrows: false
-  // });
+
+  const swiper = new Swiper('.customer-reviews__container', {
+  loop: false,
+  speed: 1200,
+  spaceBetween: 30,
+  loop: true,
+  speed: 800,
+  navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+    el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true
+    },
+  });
 
   var mixer = mixitup('.popular-categories__products');
 });
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/swiper/swiper-bundle.js',
     'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
   ])
